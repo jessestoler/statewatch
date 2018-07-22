c697e181cead2b6f944d665bb843f01dadab5a65
Jesse Stoler <j.sto@Jesses-MacBook-Pro.local>
2018-07-21 19:08:29 -0600

    semicolon

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', function(req, res) {
       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-  }
+  };
 
 
 
