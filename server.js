2c80f67c89a4c274f71873b539a71327b39fcac2
Jesse Stoler <j.sto@Jesses-MacBook-Pro.local>
2018-07-21 18:13:27 -0600

    heroku stuff

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const db = require("./models");
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 
 
@@ -15,7 +16,6 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 
 
