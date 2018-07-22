a0c327dabf0e49e9199b137e77772d7d7ed2e2d9
Jesse Stoler <j.sto@Jesses-MacBook-Pro.local>
2018-07-21 18:44:22 -0600

    server again

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-if (process.env.NODE_ENV === 'production') {
+/*if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-   // app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-  }
+  }*/
 
 
 
